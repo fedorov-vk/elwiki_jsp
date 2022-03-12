afe2eddf041c92d91302797c435f1437008ffe19
Victor Fedorov <fedorov.vk@yandex.ru>
2022-03-12 12:52:28 +0300

    change: maven pom - введение профилей 'linux', 'windows'.

--- elwikijsp.root/build(no_testing).sh
+++ elwikijsp.root/build(no_testing).sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 #
 
-mvn install  -Dmaven.test.skip=true  $* | tee Build.log
+#mvn install        -Dmaven.test.skip=true  $* | tee Build.log
+mvn install -Plinux -Dmaven.test.skip=true  $* | tee Build.log
