17d2a0d82318f4bd3c58e5255ce3b074489fecb2
Victor Fedorov <fedorov.vk@yandex.ru>
2022-04-29 22:03:00 +0300

    настройка пула p2 репозиториев

---
+++ elwikijsp.root/repositories_list.sh
@@ -0,0 +1,6 @@
+export PLATFORM_REPO=/repo/eclipse/releases/2021-12
+export APACHETOOLS_REPO=/home/vfedorov/dev/dev_wikijsp/GIT/elwiki+jsp_2021-12/p2_repositories/apacheTools/target/repository
+export CUSTOM_REPO01=/home/vfedorov/dev/dev_wikijsp/GIT/elwiki+jsp_2021-12/p2_repositories/custom-p2-site/target/repository
+export CUSTOM_REPO02=/home/vfedorov/dev/dev_wikijsp/GIT/elwiki+jsp_2021-12/p2_repositories/additionalArtifacts/target/repository
+export CUSTOM_REPO03=/home/vfedorov/dev/dev_wikijsp/GIT/elwiki+jsp_2021-12/p2_repositories/fromEclipse/target/repository
+export CUSTOM_REPO04=/home/vfedorov/dev/dev_wikijsp/GIT/elwiki+jsp_2021-12/p2_repositories/for-test-site/target/repository
--- elwikijsp.root/verify(no_testing).sh
+++ elwikijsp.root/verify(no_testing).sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 #
-
+. ./repositories_list.sh
 #mvn install        -Dmaven.test.skip=true  $* | tee Build.log
-mvn verify -Plinux -Dmaven.test.skip=true  $* | tee Build.log
+mvn verify -Plinux -Dmaven.test.skip=true $* | tee Build.log
