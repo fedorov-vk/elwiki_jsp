044be330539b1644d7b43a1b90c51e7962f9d16b
Victor Fedorov <fedorov.vk@yandex.ru>
2022-05-31 19:48:24 +0300

    improving scripts placement.

--- p2_repositories/make_offline_repo.cmd
+++ p2_repositories/make_offline_repo.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-call %~dp0\..\maven_settings.cmd
+call %~dp0\..\utilities\maven_settings.cmd
 
 mvn %MAVEN_SETTINGS% ^
  p2:site | tee Build.log
--- p2_repositories/make_online_repo.cmd
+++ p2_repositories/make_online_repo.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-call %~dp0\..\maven_settings.cmd
+call %~dp0\..\utilities\maven_settings.cmd
 
 mvn %MAVEN_SETTINGS% ^
  jetty:run %* | tee Build.log
