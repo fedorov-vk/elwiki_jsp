d237397615621531c7b9372cf88a97939792c4bd
v.fedorov <v.fedorov@USER77.intranet-sintek.net>
2022-05-31 12:52:12 +0300

    add utilities directory; beautify .target definitions

---
+++ utilities/doMirror_2021-12.cmd
@@ -0,0 +1,18 @@
+@echo off
+REM Creates a local copy of the Eclipse p2 repository.
+REM The repository URL desines SRC, for example:
+REM   https://download.eclipse.org/releases/2019-03/201902081000/
+REM The placement directory defines DST. (at running directory - will be created %DST%)
+REM ECLIPSE - determines the placement of the starter of the installed Eclipse.
+REM OPTS - defines additional startup parameters.
+ 
+set ECLIPSE=D:\PROGRA~2\Eclipse\eclipse_rcp_2021-12-R\eclipse.exe
+set OPTS=-nosplash -verbose
+set SRC=https://download.eclipse.org/releases/2021-12
+set DST=2021-12
+
+echo Get from: %SRC%
+echo   Put to: %DST%
+
+%ECLIPSE% %OPTS% -application org.eclipse.equinox.p2.metadata.repository.mirrorApplication -source %SRC%  -destination %DST%
+%ECLIPSE% %OPTS% -application org.eclipse.equinox.p2.artifact.repository.mirrorApplication -source %SRC%  -destination %DST%
