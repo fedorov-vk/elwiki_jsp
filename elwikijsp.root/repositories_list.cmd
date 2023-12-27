09e202384d6a453826bd7dbbf3b3ec5343c79f6f
vfedorov <fedorov.vk@yandex.ru>
2023-12-27 14:17:33 +0300

    building reviewed.

--- elwikijsp.root/repositories_list.cmd
+++ elwikijsp.root/repositories_list.cmd
@@ -6,13 +6,11 @@ set PLATFORM_REPO=%USERPROFILE%/p2/eclipse/releases/2022-12
 
 set P2_REPO_PLACE=%~dp0\..\p2_repositories
 :: APACHETOOLS_REPO - probably not used anymore.
-set APACHETOOLS_REPO=%P2_REPO_PLACE%\apache-tools\target\repository
-set CUSTOM_REPO01=%P2_REPO_PLACE%\custom-p2-site\target\repository
-set CUSTOM_REPO02=%P2_REPO_PLACE%\additional-artifacts\target\repository
-set CUSTOM_REPO03=%P2_REPO_PLACE%\from-eclipse\target\repository
+set CUSTOM_REPO01=%P2_REPO_PLACE%\apache-tools\target\repository
+set CUSTOM_REPO02=%P2_REPO_PLACE%\custom-p2-site\target\repository
+set CUSTOM_REPO03=%P2_REPO_PLACE%\additional-artifacts\target\repository
 set CUSTOM_REPO04=%P2_REPO_PLACE%\for-test-site\target\repository
 
-set "APACHETOOLS_REPO=%APACHETOOLS_REPO:\=/%"
 set "CUSTOM_REPO01=%CUSTOM_REPO01:\=/%"
 set "CUSTOM_REPO02=%CUSTOM_REPO02:\=/%"
 set "CUSTOM_REPO03=%CUSTOM_REPO03:\=/%"
--- elwikijsp.root/repositories_list.sh
+++ elwikijsp.root/repositories_list.sh
@@ -5,9 +5,8 @@ export PLATFORM_REPO=/repo/eclipse/releases/2022-12
 
 P2_REPO_PLACE=$PWD/../p2_repositories
 # APACHETOOLS_REPO - probably not used anymore.
-export APACHETOOLS_REPO=$P2_REPO_PLACE/apache-tools/target/repository
-export CUSTOM_REPO01=$P2_REPO_PLACE/custom-p2-site/target/repository
-export CUSTOM_REPO02=$P2_REPO_PLACE/additional-artifacts/target/repository
-export CUSTOM_REPO03=$P2_REPO_PLACE/from-eclipse/target/repository
+export CUSTOM_REPO01=$P2_REPO_PLACE/apache-tools/target/repository
+export CUSTOM_REPO02=$P2_REPO_PLACE/custom-p2-site/target/repository
+export CUSTOM_REPO03=$P2_REPO_PLACE/additional-artifacts/target/repository
 export CUSTOM_REPO04=$P2_REPO_PLACE/for-test-site/target/repository
 export JAVADOC=$PWD/../javadoc/
